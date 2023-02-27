166d27981ad4e765926f3f89054fd4ff115a9389
Jasmine Molano <jasmine.molanocorreia@heig-vd.ch>
2023-02-27 09:55:17 +0100

    test

--- src/components/TheGround.vue
+++ src/components/TheGround.vue
@@ -4,6 +4,6 @@
 
 <template>
     <a-entity>
-        <a-circle color="white" radius="20" rotation="-90 0 0"></a-circle>
+        <a-circle color="blue" radius="20" rotation="-90 0 0"></a-circle>
     </a-entity>
 </template>
